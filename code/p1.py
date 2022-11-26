7de864a248f99b4933119b6b9575270975d75072
Varul Srivastava <varul.srivastava@research.iiit.ac.in>
2022-11-26 21:32:33 +0530

    added code

--- code/p1.py
+++ code/p1.py
@@ -3,12 +3,15 @@ import twint
 nest_asyncio.apply()
 
 config = twint.Config()
-config.Search = "btc"
-config.Lang = "en"
-config.Since = '2016-01-01 00:00:00'
-config.Until = '2016-01-26 00:00:00'
+config.Search = "Ukraine"
+#config.Location = True
+#config.Geo = "48.37,31.16,100"
+#config.Lang = "en"
+config.Since = '2022-04-01 00:00:00'
+config.Until = '2022-05-01 00:00:00'
+config.Limit = 1000
 config.Store_csv = True
-config.Output = "BTCtweet.csv"
+config.Output = "UkraineData.csv"
 #running search
 twint.run.Search(config)
 
